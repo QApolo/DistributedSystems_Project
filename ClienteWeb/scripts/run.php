febb0d58789d0717ed801ad7fe81e3362e71073d
Diego Puente <21991012+DiegoPGs@users.noreply.github.com>
2019-11-29 11:24:17 -0600

    Update run.php

--- ClienteWeb/scripts/run.php
+++ ClienteWeb/scripts/run.php
@@ -1,8 +1,17 @@
 <?php
   $filename = $_GET['nFile'];
-  $salida =  shell_exec('ls -lart');
+  //$salida =  shell_exec('ls -lart');
   //$salida =  shell_exec('./Cliente '.$filename);
+
+  //$salida = shell_exec('(cd ..; /CarpetaPruebas/prueba)');
+
+  $cmd = "./prueba $filename";
+
+  var_dump($cmd);
+
+  $salida = shell_exec( "(cd ../../CarpetaPruebas; $cmd)" );
+
   //echo "<pre>$salida</pre>";
-  //var_dump($filename);
+  //print_r($salida);
   var_dump($salida);
 ?>
